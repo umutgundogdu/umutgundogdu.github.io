b0ff7f00b04dc8e80ed8a3d7a09e0b4568fe4e9e
umutgundogdu <umutgundogdu@users.noreply.github.com>
2022-06-30 18:46:44 +0000

    deploy: 3248ee3434b2aa3f424a468b93a645d327b164cc

---
+++ _next/static/bEhUyB-UBTK7FOXIndi7N/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b3eb926c94c7801e.css","static/chunks/pages/index-0e00e9c447606c4d.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
